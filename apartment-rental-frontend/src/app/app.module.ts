2b6cf4ef068b6ba1c5e0b83d7229efce5774dbd7
Vdhakal <vdhakal10@gmail.com>
2021-09-10 11:56:14 -0500

    first

---
+++ apartment-rental-frontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const appRoutes: Routes = [
+  {path: '', component:PropertyListComponent},
+  {path: 'add-strippers', component:AddPropertyComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
+    AddPropertyComponent
+   ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ apartment-rental-frontend/src/app/services/housing.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+import { IProperty } from '../property/IProperty.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllProperties(){
+    return this.httpClient.get("data/properties.json").pipe(
+      map(data =>{
+        const propertiesArray: Array<IProperty>=[];
+        for(const id in data){
+          if(data.hasOwnProperty(id)){
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    )
+  }
+}
